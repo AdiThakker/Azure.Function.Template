e7a816f0e190d005771d4d48a8744418fed10bfd
Adi Thakker <Aditya.Thakker@gmail.com>
2022-02-18 16:07:01 -0500

    Add project files.

---
+++ templates/Azure.FunctionServiceTemplate.Scaffolding/Source/Company.Application.Component.Service/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly:FunctionsStartup(typeof(Company.Application.Component.Service.Startup))]
+namespace Company.Application.Component.Service
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // TODO Register Dependencies
+            throw new System.NotImplementedException();
+        }
+    }
+}
